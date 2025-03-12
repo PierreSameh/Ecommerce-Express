07842d957febc69d45a8b37dc362f744fb8147c4
PierreSameh <captainpirzon@gmail.com>
2025-03-12 09:33:52 +0200

    feat: update environment configuration and enhance product image validation

--- src/validations/productValidation.js
+++ src/validations/productValidation.js
@@ -2,7 +2,13 @@ import { body } from "express-validator";
 
 export const validateStore = [
   body("title").notEmpty().withMessage("Title is Required"),
-  //   body("images").().withMessage("Image is Required"),
   body("price").notEmpty().withMessage("Price is Required"),
   body("stock").notEmpty().withMessage("Stock is Required"),
+
+  body("images").custom((value, { req }) => {
+    if (!req.files || req.files.length === 0) {
+      throw new Error("At least one image is required");
+    }
+    return true;
+  }),
 ];
